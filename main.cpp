5a322a3be6f1a1d26bbfaa78aeeca5bada4306d1
Michael Uman <muman613@gmail.com>
2012-09-24 22:12:44 -0700

    Cleanup old test code from gdplusplus library.

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@
 #include "gd_stubs.h"
 
 using namespace std;
-//BGD_DECLARE(gdImagePtr) gdImageCreateFromJpeg (FILE * infile);
 
-//typedef gdImagePtr (*_gdImageCreateFromJpeg)(FILE*);
+/**
+ *
+ */
 
 int main()
 {
-#if 1
     gdLibrary gd;
 
     FILE* fp = fopen("image.jpg", "r");
@@ -27,31 +27,6 @@ int main()
     gd.gdImagePng( image, fp );
 
     fclose(fp);
-
-#else
-    void* lib = 0L;
-    _gdImageCreateFromJpeg gdImageCreateFromJpeg;
-
-    cout << "Hello world!" << endl;
-
-    lib = dlopen("libgd.so", RTLD_NOW);
-    if (lib == 0) {
-        printf("ERROR: %s\n", dlerror());
-        return -1;
-    }
-    printf("lib = %p\n", lib);
-
-    gdImageCreateFromJpeg = (_gdImageCreateFromJpeg)dlsym( lib, "gdImageCreateFromJpeg" );
-    assert(gdImageCreateFromJpeg != 0);
-
-    FILE* fp = fopen("image.jpg", "r");
-    gdImagePtr image = gdImageCreateFromJpeg( fp );
-    fclose(fp);
-
-    printf("image = %p\n", image);
-
-    dlclose( lib );
-#endif
 
     return 0;
 }
